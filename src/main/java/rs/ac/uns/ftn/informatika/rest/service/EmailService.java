036800e11833a793e401cd296627fb1943b7af2c
mark-ristic <risticmark1337@gmail.com>
2021-05-22 11:30:28 +0200

    Removed plain text credentials - switched to Env Variables, discarded leaked creds

--- src/main/java/rs/ac/uns/ftn/informatika/rest/service/EmailService.java
+++ src/main/java/rs/ac/uns/ftn/informatika/rest/service/EmailService.java
@@ -1,17 +1,21 @@
 package rs.ac.uns.ftn.informatika.rest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
 
 @Service
 public class EmailService {
 
     @Autowired
-    private JavaMailSender emailSender;
+    JavaMailSender emailSender;
 
     public boolean sendSimpleMessage(String to, String subject, String text) {
 
