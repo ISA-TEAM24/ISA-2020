e0d0d249ee82a9ba35ac684e3fec2d43c63e15c8
nikola-tomik <nikola.tomik12@gmail.com>
2021-06-04 15:13:02 +0200

    Implemented filling pharmacist working calendar with upcoming/past visits and added option for giving penalty inside visit

--- src/main/webapp/js/pharmacist/kalendar.js
+++ src/main/webapp/js/pharmacist/kalendar.js
@@ -1,20 +1,33 @@
-var datas = [];
+datas = [];
 
 $(document).ready(function() {
-
+    
 })
-  
-  window.addEventListener('load', addEventRenderCalendar(), false);
-  
-  function addEventRenderCalendar() {
-    //addEventToCalendar(obj);
-    initCalendar(datas)
-  }
-  
-  function initCalendar(datas) {
-    document.addEventListener('DOMContentLoaded', function() {
-  
-      
+
+// on window load popuni kalendar
+window.addEventListener('load', addEventRenderCalendar(), false);
+
+function addEventRenderCalendar() {
+  datas = [];
+  console.log('PRE UPISA STANJE DATA: ');
+  console.log(datas);
+
+  initCalendar(datas);
+
+}
+
+function initCalendar(datas) {
+
+  console.log('INIT CALENDAR');
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    getUpcomingVisits();
+    getPastVisits();
+    
+    var millisecondsToWait = 500;
+    setTimeout(function() {
+
       var calendarEl = document.getElementById('calendar');
       var calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
@@ -24,25 +37,120 @@ $(document).ready(function() {
           center: 'title',
           right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
         },
-        defaultView: 'dayGridMonth',
+        defaultView: 'listMonth',
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         eventLimit: true, // allow "more" link when too many events
         events: datas
-      });
-   
+        });
+
       calendar.render();
-    });
   
-  
-  }
-  
-  function addEventToCalendar(obj) {
-    datas.push(obj);
-  }
-  
-  // initCalendar(datas);
-  
-  
-  
-  
+    }, millisecondsToWait);
+
+    console.log('POSLE RENDEROVANJE: ');
+    console.log(datas);
+  });
+
+}
+
+
+function getUpcomingVisits() {
+  $.ajax({
+      type:'GET',
+      url: '/api/visit/getupcomings',
+      contentType : 'application/json',
+      beforeSend: function (xhr) {
+          xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('myToken'));
+      },
+      success : function(data) {
+          console.log(data)
+          addUpcomingVisitsToCalendar(data);
+
+      },
+      error : function() {
+          console.log('Cant get upcoming visits');
+      }    
+  })
+}
+
+function addUpcomingVisitsToCalendar(data) {
+    data.forEach(function(visit) {
+
+      var startTime = visit.datum + "T" + visit.vreme + ":00";
+
+      var startDate = new Date(startTime);
+      console.log(startDate);
+      
+      console.log(visit.trajanje.toString());
+      var endTime = new Date(startDate.setMinutes(startDate.getMinutes() + visit.trajanje ));
+      
+      console.log(endTime);
+
+      var origin = window.location.origin;
+      console.log(origin + "/pharmacist/aktivnoSavetovanje.html");
+      var visitUrl = origin + "/pharmacist/aktivnoSavetovanje.html?" + parseInt(visit.id);
+      
+      var event = {
+        title: "Savetovanje: " + visit.ime + " " + visit.prezime,
+        start: startTime,
+        end : endTime,
+        url : visitUrl,
+        color: '#37c5cc'
+      } 
+
+      datas.push(event);
+    })
+
+}
+
+function getPastVisits() {
+  $.ajax({
+    type:'GET',
+    url: '/api/visit/getfinishedvisits',
+    contentType : 'application/json',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('myToken'));
+    },
+    success : function(data) {
+        console.log(data);
+        addPastVisitsToCalendar(data)
+    },
+    error : function() {
+        console.log('Cant get finished visits');
+    }    
+  })
+}
+
+function addPastVisitsToCalendar(data) {
+    data.forEach(function(visit) {
+
+      var startTime = visit.datum + "T" + visit.vreme + ":00";
+
+      var startDate = new Date(startTime);
+      console.log(startDate);
+      
+      console.log(visit.trajanje.toString());
+      var endTime = new Date(startDate.setMinutes(startDate.getMinutes() + visit.trajanje ));
+      
+      console.log(endTime);
+
+      var origin = window.location.origin;
+      var visitUrl = origin + "/pharmacist/pregledaniKlijentiFarmaceut.html";
+      
+      var event = {
+        title: "Savetovanje: " + visit.pacijentIme + " " + visit.pacijentPrezime,
+        start: startTime,
+        end : endTime,
+        url : visitUrl,
+        color: '#eb8c21'
+      } 
+
+      datas.push(event);
+  })
+}
+
+
+
+
+
