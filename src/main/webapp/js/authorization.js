201c052355058238c3d5fe4193850450ab3bec61
nikola-tomik <nikola.tomik12@gmail.com>
2021-06-05 10:57:31 +0200

    added client side protection for dermatologist and pharmacist pages

--- src/main/webapp/js/authorization.js
+++ src/main/webapp/js/authorization.js
@@ -5,6 +5,16 @@ const phadmin_check = ['pharmacyadmin/absencerequestpage.html', 'pharmacyadmin/a
 
 const patient_check = ['user/me.html']
 
+const dermatologist_check = ['dermatologist/aktivanPregled.html', 'dermatologist/indexDermatolog.html',
+                    'dermatologist/mojProfilDermatolog.html', 'dermatologist/pregled.html',
+                    'dermatologist/pregledaniKlijenti.html', 'dermatologist/radniKalendar.html',
+                    'dermatologist/timeOffZahtev.html', 'dermatologist/zakaziPregled.html']
+
+const pharmacist_check = ['pharmacist/aktivnoSavetovanje.html', 'pharmacist/indexFarmaceut.html',
+                    'pharmacist/izdavanjeLeka.html', 'pharmacist/mojProfilFarmaceut.html',
+                    'pharmacist/pregledaniKlijentiFarmaceut.html', 'pharmacist/radniKalendar.html',
+                    'pharmacist/savetovanje.html', 'pharmacist/timeOffZahtev.html', 'pharmacist/zakaziSavetovanje.html']
+
 $(document).ready(function() {
     
     checkAuthority()
@@ -13,10 +23,21 @@ $(document).ready(function() {
 function checkAuthority() {
 
     var url = window.location.pathname.substring(1);
+    console.log(url)
 
     if(patient_check.includes(url)) {
         console.log('patient includes')
         runPatientCheck()
+    }
+
+    if(dermatologist_check.includes(url)) {
+        console.log('dermatologist includes')
+        runDermatologistCheck()
+    }
+
+    if(pharmacist_check.includes(url)) {
+        console.log('pharmacist includes')
+        runPharmacistCheck()
     }
 
 }
@@ -60,3 +81,87 @@ function checkPatientAuthority(user) {
 } 
 
 // patient check end
+
+// dermatologist check start
+
+function runDermatologistCheck() {
+
+    $.ajax({
+        type:'GET',
+        url: '/api/whoami',
+        contentType : 'application/json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('myToken'));
+        },
+        success : function(user) {
+            checkDermatologistAuthority(user)
+        },
+        error : function() {
+            console.log('An Error has occured while trying to reload the profile')
+            //alert('Error message', 'Could not load profile information.')
+            window.location.href = '../index.html'
+        }
+        
+    })
+}
+
+function checkDermatologistAuthority(user) {
+
+    console.log(user)
+    // presume that we shouldn't be allowed
+    var shouldReturn = true
+    user.authorities.forEach(function(a) {
+        if(a.authority == 'ROLE_DERMATOLOGIST') {
+            shouldReturn = false
+        }
+    })
+
+    if (shouldReturn) {
+        window.location.href = '../index.html'
+    }
+    console.log('You are a dermatologist, you are allowed on this page')
+} 
+
+// dermatologist check end
+
+// pharmacist check start
+
+function runPharmacistCheck() {
+
+    $.ajax({
+        type:'GET',
+        url: '/api/whoami',
+        contentType : 'application/json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('myToken'));
+        },
+        success : function(user) {
+            checkPharmacistAuthority(user)
+        },
+        error : function() {
+            console.log('An Error has occured while trying to reload the profile')
+            //alert('Error message', 'Could not load profile information.')
+            window.location.href = '../index.html'
+        }
+        
+    })
+}
+
+function checkPharmacistAuthority(user) {
+
+    console.log(user)
+    // presume that we shouldn't be allowed
+    var shouldReturn = true
+    user.authorities.forEach(function(a) {
+        if(a.authority == 'ROLE_PHARMACIST') {
+            shouldReturn = false
+        }
+    })
+
+    if (shouldReturn) {
+        window.location.href = '../index.html'
+    }
+    console.log('You are a pharmacist, you are allowed on this page')
+} 
+
+// pharmacist check end
